38eb2ed225f3b833d2333e5ee2fe5c3d26e9c9df
Mystic-rr <840502601@qq.com>
2024-05-09 01:12:25 +0800

    语音识别添加，聊天历史添加

--- src/app/chat-page/chat-page.component.ts
+++ src/app/chat-page/chat-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, Renderer2, ViewChild, input } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, Input, Renderer2, ViewChild, input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzInputModule } from 'ng-zorro-antd/input';
@@ -8,25 +8,35 @@ import { ChatResponse, Message, ViewMessage } from '../interface/chat-interface'
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { CommonModule } from '@angular/common';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
-
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { HistoryPageComponent } from '../history-page/history-page.component';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzMessageService } from 'ng-zorro-antd/message';
 @Component({
   selector: 'app-chat-page',
   standalone: true,
-  imports: [FormsModule, NzButtonModule, NzInputModule, ChatContentComponent,NzIconModule,CommonModule,NzDividerModule],
+  imports: [FormsModule, NzButtonModule, NzInputModule, ChatContentComponent, NzIconModule, CommonModule, NzDividerModule, NzDrawerModule, HistoryPageComponent, NzMessageModule],
   templateUrl: './chat-page.component.html',
   styleUrl: './chat-page.component.css'
 })
 export class ChatPageComponent {
 
   @ViewChild('inputarea') inputArea!: ElementRef
-  @Input() query!: string
+  @ViewChild(ChatContentComponent) private chatContent!: ChatContentComponent
+  @Input() query: string=''
 
-  constructor(private renderer: Renderer2, private chatService: ChatServiceService) { }
+  
+  constructor(private renderer: Renderer2, private chatService: ChatServiceService, private errorMessage: NzMessageService,private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
   }
 
   messages = new Array<ViewMessage>
+  historyMessages = new Array<Array<ViewMessage>>
+
+  // 控制history内容可见
+  visible = false;
+
   sort = 0
 
   inputAreaFocus() {
@@ -45,19 +55,28 @@ export class ChatPageComponent {
   }
 
   sendQuery() {
-    console.log('query start ')
-    if(this.query !== '' && this.query !== null){
+    if (this.query.trim() !== '' && this.query.trim() !== null) {
+      console.log('query start ')
       let queryContent = this.query
       this.query = ''
       this.messages.push({ content: queryContent, role: 'user', sort: this.sort++ })
-      this.chatService.sendQuery(queryContent).subscribe(
-        (res) => {
+      this.chatContent.isloading = true
+      this.chatService.sendQuery(this.messages).subscribe({
+        next: (res) => {
           let data = res as ChatResponse
+          this.chatContent.isloading = false
           this.messages.push({ content: data.choices[0].message.content, role: data.choices[0].message.role, sort: this.sort++ })
+        },
+        error: err => {
+          this.chatContent.isloading = false
+          this.errorMessage.error(err.error.error.message, { nzDuration: 5000 })
+          console.log(err)
         }
+      }
+
       )
     }
-    
+
   }
 
   handleKeyDown(event: KeyboardEvent) {
@@ -69,7 +88,7 @@ export class ChatPageComponent {
       console.log('Enter key pressed without Alt key.')
       this.sendQuery()
     }
-  
+
     // 如果同时按下了Alt键和回车键
     if (event.key === 'Enter' && event.altKey) {
       // 执行换行或其他操作，但默认的textarea行为已经允许Alt+Enter换行，所以可能不需要额外操作
@@ -77,8 +96,79 @@ export class ChatPageComponent {
     }
   }
 
-  clear(){
+  clear() {
     this.query = ''
+    if (this.messages.length != 0) {
+      this.historyMessages.push(this.messages)
+    }
+    this.messages = []
+  }
+  open(): void {
+    this.visible = true
+  }
+
+  closeChatHistory(): void {
+    this.visible = false
+  }
+
+  showChatHistory() {
+    this.visible = true
+  }
+
+  loadHistoryMessage(index: number) {
+    this.clear()
+    this.messages = this.historyMessages[index]
+    this.historyMessages.splice(index, 1)
+  }
+
+  recognition = new ((window as any).webkitSpeechRecognition )()
+  isRecognit = false
+
+  recogStart() {
+    // 检查浏览器是否支持 Web Speech API
+    if ('webkitSpeechRecognition' in window) {
+      console.log('recognition start')
+      // 创建一个语音识别的实例
+      this.isRecognit = true
+      this.recognition.lang = 'en-US';
+      // this.recognition.lang = 'zh-CN';
+      this.recognition.continuous = true; // 或者设置为 true 以保持不断的监听
+      this.recognition.interimResults = true; // 是否返回临时结果
+
+      // 开始语音识别
+      this.recognition.start();
+
+      // 当识别到结果时
+      this.recognition.onresult = (event: any) => {
+        let transcript = ''
+        for (let i = event.resultIndex; i < event.results.length; i++) {
+          transcript += event.results[i][0].transcript; // 获取结果
+        }
+        this.query = transcript
+        this.cdr.detectChanges();
+        console.log(transcript)
+      };
+
+      // 处理错误
+      this.recognition.onerror = (event: any) => {
+        console.error(event);
+      };
+
+      // 监听结束事件
+      this.recognition.onend = () => {
+        // 你可以在这里重新启动识别器或进行其他操作
+        console.log('Speech recognition service disconnected');
+        this.recognition.stop();
+      };
+    } else {
+      console.error('此浏览器不支持 Web Speech API');
+      this.isRecognit = false
+    }
+  }
+
+  recogStop(){
+    this.recognition.stop()
+    this.isRecognit = false
   }
 
 }
--- src/app/service/chat-service.service.ts
+++ src/app/service/chat-service.service.ts
@@ -3,7 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Token } from '@angular/compiler';
-import { Message, ChatEvent, ChatResponse } from '../interface/chat-interface';
+import { Message, ChatEvent, ChatResponse, ViewMessage } from '../interface/chat-interface';
 
 const url = "https://fukui-kankou-multi-language.openai.azure.com/openai/deployments/Fukui-Kankou-Multi-Language-35-Turbo/chat/completions?api-version=2024-02-01"
 const token = "aab8659bd4964a688c3d717e28b58aa5"
@@ -19,13 +19,16 @@ export class ChatServiceService {
 
   constructor(private http: HttpClient) { }
 
-  sendQuery(query: string){
+  sendQuery(messages: Array<ViewMessage>){
     let headers = new Headers();
     headers.append('api-key', token)
     headers.append('Content-Type', 'application/json')
-    let array = new Array<Message>
-    array.push({role:'user',content:query})
-    let jsonbody:ChatEvent = new ChatEvent(array)
+    let sendMessages = new Array<Message>
+
+    for(let message of messages){
+      sendMessages.push({role:message.role,content:message.content})
+    }
+    let jsonbody:ChatEvent = new ChatEvent(sendMessages)
     return this.http.post(url,jsonbody,{headers:{"api-key":token,"Content-Type":"application/json"}})
 
   }
